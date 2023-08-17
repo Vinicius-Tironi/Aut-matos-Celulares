a16580b9f94764225675d8e202e612cb1ea30236
Vinícius Tironi <vinicius.tironi@unesp.br>
2023-08-17 17:35:46 -0300

    Uma curva, refletida em dois anos

--- Função Sazonal.py
+++ Função Sazonal.py
@@ -32,7 +32,7 @@ def gauss_f(x, A, mu, sigma):
 popt, pcov = curve_fit(gauss_f, x , y)
 
 A_opt, mu_opt, sigma_opt = popt
-x_model = np.linspace(min(x), max(x), 730)
+x_model = np.linspace(min(x), max(x), 365)
 y_model = gauss_f(x_model, A_opt, mu_opt, sigma_opt)
 
 max_y = max(y_model)
@@ -42,35 +42,36 @@ for i in y_model:
     j = i/max_y
     y_model_norm.append(j)
     
+norm_crr_1 = []
+for i in y_model_norm:
+    j = i - 0.369
+    norm_crr_1.append(j)
 
-#plt.plot(x_model, y_model_norm, color = 'r')
-#plt.show()
-
+norm_crr_2 = []
+for i in norm_crr_1:
+    norm_crr_2.append(i)
+del norm_crr_2[205:]
+for i in range(205,365):
+    norm_crr_2.append(0)
+    
 y_model_ref = []
-for i in y_model_norm:
+for i in norm_crr_2:
     y_model_ref.append(i)
-del y_model_ref[410:]
-for i in y_model_norm:
-    y_model_ref.append(i)    
-del y_model_ref[730:]
-
-odio_raiva_e_destruicao = []
-for i in y_model_ref:
-    j = i - 0.35
-    odio_raiva_e_destruicao.append(j)
-
-
-X = []
-for i in x_model:
-    j = i + 1
-    X.append(j)
+del y_model_ref[365:]
+for i in norm_crr_2:
+    y_model_ref.append(i)
     
 
+X = np.arange(0,730)
 
-plt.plot(X, odio_raiva_e_destruicao, color = 'r')
-plt.show()
-plt.plot(x_model, odio_raiva_e_destruicao, color = 'b')
-plt.show()
+
+plt.plot(X, y_model_ref, color = 'r')
+
+
+#plt.plot(X, odio_raiva_e_destruicao, color = 'r')
+#plt.show()
+#plt.plot(x_model, norm_crr_1, color = 'b')
+#plt.show()
 
 
 
